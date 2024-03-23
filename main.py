ba8372b421f13bc3fe15111c396cae260d3a9724
Tanmay <mail2tanmaygarg@gmail.com>
2024-03-23 15:58:18 -0500

    get_teams msg send as an embed

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 client.remove_command("help")
 client.command_prefix = prefix
 
+comp_code = "2024wimi"
+url = "https://www.thebluealliance.com/event/" + comp_code + "#teams"
+pit_response = requests.get(url, headers={"X-TBA-Auth-Key": tokens.blueAllianceToken})
+pit_soup = BeautifulSoup(pit_response.text, "html.parser", parse_only=SoupStrainer('div', class_='team-name'))
+teams_list = pit_soup.find_all('a')
+
+teams = {
+    int(re.search(r'/team/(\d+)/', str(team)).group(1)): re.search(r'>(\d+)<br/>(.*)</a>',
+    str(team)).group(2) for team in teams_list}
+
+scouters = []
 
 @client.event
 async def on_ready():
@@ -19,22 +30,25 @@ async def on_ready():
 
 
 @client.command()
+async def scouters(ctx, arg):
+    scouters = arg.split(",")
+    await ctx.send("Scouters Set!")
+
+
+@client.command()
 async def get_teams(ctx):
-    url = "https://www.thebluealliance.com/event/" + "2024wimi" + "#teams"
-    pit_response = requests.get(url, headers={"X-TBA-Auth-Key": tokens.blueAllianceToken})
-    pit_soup = BeautifulSoup(pit_response.text, "html.parser", parse_only=SoupStrainer('div', class_='team-name'))
-    teams_list = pit_soup.find_all('a')
-
-    teams = {
-        int(re.search(r'/team/(\d+)/', str(team)).group(1)): re.search(r'>(\d+)<br/>(.*)</a>', str(team)).group(2)
-        for team in teams_list}
-
-    await ctx.send(teams)
+    team_items = list(teams.items())
+    for i in range(0, len(team_items), 25):
+        send_embed = discord.Embed(title=f"Teams at {comp_code}", description="List of teams", color=0xeabfff)
+        for team_number, team_name in team_items[i:i + 25]:
+            if len(team_name) > 1024:
+                team_name = team_name[:1021] + "..."
+            send_embed.add_field(name=team_number, value=team_name, inline=False)
+        await ctx.send(embed=send_embed)
 
 
 @client.command
 async def assign_pit(ctx, arg):
-    scouters = arg.split(",")
     for scout in scouters:
         pass
     await ctx.send("Pit Scouting Schedule Set!")
@@ -42,7 +56,6 @@ async def assign_pit(ctx, arg):
 
 @client.command()
 async def assign_match(ctx, arg):
-    scouters = arg.split(",")
     for scout in scouters:
         pass
     await ctx.send("Match Scouting Schedule Set!")
