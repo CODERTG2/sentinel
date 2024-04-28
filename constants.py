41749c69447531b45ff87635495726b3fd9c63cf
Tanmay <mail2tanmaygarg@gmail.com>
2024-04-28 09:31:01 -0500

    env variables working!
    set APIs as env variables

--- constants.py
+++ constants.py
@@ -3,9 +3,4 @@ import os
 comp_code = "2024ilch"
 
 botToken = os.getenv("BOT_TOKEN")
-print(botToken)
-
 blueAllianceToken = os.getenv("BLUE_ALLIANCE_TOKEN")
-
-# botToken = "MTIxODMxMTQ2NzExMjA3NTM3NQ.G1PQG_.nQmH72Gz26v0J_9mzj3OOTz8AWgxP7AHs8TxAo"
-# blueAllianceToken = "PiqFQSWcSw9DgmNEhNDviFRxzfTOa8Bp2l3g5nX42ngqqi9zlaLnzizwt2gtDQQQ"
